3eb7f83266c76d28ab21a28c4fc41f5d83750d7a
Arjun Rubalingam <arjun.ruba@gmail.com>
2023-02-09 19:28:22 +0530

    Create Data.R
    Added Data.R document

---
+++ R/Data.R
@@ -0,0 +1,47 @@
+#' Adverse Event datasets
+#' 
+#' Findings About Clinical Event dataset
+#' 
+#' A Mock SDTM FACE dataset
+#' 
+#' @source fa_dummy.R
+#' 
+#' "admiralvaccine_face.rda"
+#' 
+#' Clinical Event datasets
+#' 
+#' A Mock SDTM CE dataset
+#' 
+#' @source ce_dummy.R
+#' 
+#' "admiralvaccine_ce.rda"
+#' 
+#' A Mock SDTM AE dataset
+#' 
+#' @source ae_dummy.R
+#' 
+#' "admiralvaccine_ae.rda"
+#' 
+#' Demography datasets
+#' 
+#' A Mock SDTM DM dataset
+#' 
+#' @source dm_dummy.R
+#' 
+#' "admiralvaccine_dm.rda"
+#' 
+#' Exposure datasets
+#' 
+#' A Mock SDTM EX dataset
+#' 
+#' @source ex_dummy.R
+#' 
+#' "admiralvaccine_ex.rda"
+#' 
+#' Vital signs datasets
+#' 
+#' A Mock SDTM VS dataset
+#' 
+#' @source vs_dummy.R
+#' 
+#' "admiralvaccine_vs.rda"
